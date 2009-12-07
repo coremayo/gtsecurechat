044fa4ee7e01519333fd63f265db47b62a3ed19a
coremayo <coremayo@bf0e2042-df83-11de-a6a9-d5aeed48c2a6>
2009-12-07 03:52:22 +0000

    removing old CS4237 dir

--- trunk/src/main/java/edu/gatech/cc/CS4237/gtsecurechat/IStreamCipher.java
+++
@@ -1,9 +0,0 @@
-package edu.gatech.cc.CS4237.gtsecurechat;
-
-public interface IStreamCipher {
-
-	void initialize(byte[] key);
-	byte[] encrypt(String plain, byte[] IV);
-	String decrypt(byte[] cipher, byte[] IV);
-	
-}
